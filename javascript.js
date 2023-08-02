f7f45e0aa46c1cb54944b85f8755d8bcdcfaa2f2
leafy47 <138401372+leafy47@users.noreply.github.com>
2023-08-02 18:22:13 +0100

    Added Button to Reset
    Can now reset to new values

--- javascript.js
+++ javascript.js
@@ -2,19 +2,61 @@ newDiv = document.createElement('div');
 newDiv.setAttribute('class', 'initialdiv');
 lesserDiv = document.createElement('div');
 lesserDiv.setAttribute('class', 'lesserdiv');
+let userInput = document.getElementById('resetform');
+let reset = document.getElementById('resetone');
+
+
 
 container = document.querySelector('.container');
 
-function firstThis () {
-    for (i = 0; i < 16; i++) {
-        lesserDiv.appendChild(newDiv.cloneNode(true));
+function createGrid (i) {
+    for (; i > 0; i--) {
+        container.appendChild(lesserDiv.cloneNode(true));
+    }
+}
+function createSquares () {
+    lesser = document.querySelectorAll('.lesserdiv');
+    for (i = 0; i < lesser.length ; i++) {
+        for(z = 0; z < lesser.length; z++) {
+            lesser[i].appendChild(newDiv.cloneNode(true));
+        }
     }
 }
 
-function startingDivs () {
-    for (let i = 0; i < 16; i++) {
-            container.appendChild(lesserDiv.cloneNode(true));
-        }
+
+createGrid(16);
+createSquares();
+turn();
+
+function turn () {
+    turnBlue = document.querySelectorAll('.initialdiv');
+    turnBlue.forEach((div) => {
+        div.addEventListener('mouseover', () => div.style.backgroundColor = 'blue')
+    });
+}
+
+
+
+
+userInput.addEventListener('submit', (event) => {
+    event.preventDefault();
+    removeDiv();
+    createGrid(reset.value);
+    createSquares();
+    turn();
+});
+
+
+
+function removeDiv () {
+    let countone = document.getElementsByClassName('initialdiv');
+    let counttwo = document.getElementsByClassName('lesserdiv');
+    for (i = 0; i < counttwo.length; i++){
+        while (counttwo[i].firstChild) {
+            counttwo[i].removeChild(counttwo[i].lastChild);
+    }}
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
-firstThis();
-startingDivs();
+    }
+
